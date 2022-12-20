329704d6bcf3c77c95a7430eb875a05a23a7cc71
Anubhav <1rn21cs026.anubhavkumar@gmail.com>
2022-12-20 12:23:50 +0530

    sieve of eratothensis

---
+++ c++ code/.vscode/lovebubber class/interviewquestion/countprime.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+        int n;
+        cin>>n;
+        int cnt=0;
+        vector<bool>prime(n+1,true);
+        prime[0]=prime[1]=false;
+        for(int i=2;i<n;i++)
+        {
+            if(prime[i])
+            {
+                cnt++;
+                for(int j=2*i;j<n;j=j+i)
+                prime[j]=0;
+            }
+        }
+        cout<<cnt;
+}
