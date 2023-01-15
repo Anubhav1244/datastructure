266dabe5ef549b16eb6c0afc441185e0174d5ff1
Anubhav1244 <1rn21cs026.anubhavkumar768@gmail.com>
2023-01-15 18:16:56 +0530

    string liked list

---
+++ stringL.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct node
+{
+    char name[20];
+    struct node *link;
+
+};
+typedef struct node *NODE;
+NODE getnode()
+{
+    NODE x;
+    x=(NODE)malloc(sizeof(struct node));
+    if(x==NULL)
+    {
+        printf("memory is not allocated\n");
+        exit(0);
+    }
+    return x;
+}
+NODE insert_front(NODE first,char ele[])
+{
+    NODE temp;
+    temp=getnode();
+    strcpy(temp->name,ele);
+    temp->link=first;
+    return temp;
+}
+NODE delte_front(NODE first)
+{
+    if (first==NULL)
+    {
+        printf("sll is empty\n");
+        return NULL;
+    }
+    NODE next,curr;
+    curr=first;
+    char name[20];
+    strcpy(name,curr->name);
+    next=first->link;
+    free(first);
+    return next;
+    
+
+}
+void display(NODE first)
+{
+    if (first==NULL)
+    {
+        printf("sll is empty\n");
+        return;
+    }
+    NODE curr;
+    
+    curr=first;
+    while (curr!=NULL)
+    {
+        printf("%s\t",curr->name);
+        curr=curr->link;
+    }
+    
+}
+int main()
+{
+    NODE first;
+    first=NULL;
+    
+    int ch;
+    char name[20];
+    while (1)
+    {
+       printf("1.insert_front,2.delte_front,3.display\n");
+       printf("enter your choice");
+       scanf("%d",&ch);
+       switch (ch)
+       {
+       case 1:  printf("enter the element\n");
+                scanf("%s",name);
+                first=insert_front(first,name);
+                break;
+        case 2: first=delte_front(first);
+                break;
+        case 3: display(first);
+                break;
+        
+       
+        default: exit(0);
+        
+       }//commit;
+    }
+    
+}
