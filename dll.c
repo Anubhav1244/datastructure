53980ce54ffb25eb3e76c7e3a9583583a2c48e1d
Anubhav1244 <1rn21cs026.anubhavkumar@gmail.com>
2023-02-10 07:56:05 +0530

    complaete doublly linkedlist

--- dll.c
+++ dll.c
@@ -79,15 +79,78 @@ NODE insert_rear(NODE first,int item)
     temp->llink=curr;
     return first;
 }
+NODE delete_front(NODE first)
+{
+    NODE next;
+    if (first==NULL)
+    {
+        printf("no element in the dll");
+        return NULL;
+    }
+    next=first->rlink;
+    if (next==NULL)
+    {
+        printf("delted item is %d\n",first->info);
+        return next;
+    }
+    next->llink=NULL;
+    printf("delted item is %d\n",first->info);
+    free(first);
+    return next;
+    
+}
+NODE delete_rear(NODE first)
+{
+    NODE curr,prev;
+    if(first==NULL)
+    {
+        printf("no element in the dll\n");
+        return NULL;
+    }
+    curr=first;
+    prev=NULL;
+    while (curr->rlink!=NULL)
+    {
+        prev=curr;
+        curr=curr->rlink;
+    }
+    printf("delted item is %d\n",curr->info);
+    (curr->llink)->rlink=NULL;
+    free(curr);
+    return first;
+
+    
+}
 int main()
 {
 
-    NODE first=NULL;
-    first=insert_front(first,22);
-    display(first);
-    first=insert_front(first,33);
-    display(first);
-    first=insert_rear(first,57);
+    NODE first;
+    first=NULL;
+    int ch,ele;
+    while (1)
+    {
+        printf("\n1.insert_front,2.insert_rear,3.delte_front,4.delte_rear,5.display,6.exit\n");
+        printf("enter the choice\n");
+        scanf("%d",&ch);
+        switch (ch)
+        {
+        case 1: printf("enter the element\n");
+                scanf("%d",&ele);
+                first=insert_front(first,ele);
+                break;
+        case 2: printf("enter the element\n");
+                scanf("%d",&ele);
+                first=insert_rear(first,ele);
+                break;
+        case 3: first=delete_front(first);
+                break;
+        case 4: first=delete_front(first);
+                break;
+        case 5: display(first);
+                break;
+        default:
+            break;
+        }
+    }
     
-    display(first);
 }
